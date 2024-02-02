6eac08df82182762776f00ae303440adbc6b7f0a
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2024-02-02 10:42:54 +0100

    Second set of rebase changes

--- lib/Interpreter/BackendPasses.cpp
+++ lib/Interpreter/BackendPasses.cpp
@@ -386,7 +386,7 @@ void BackendPasses::CreatePasses(int OptLevel, llvm::ModulePassManager& MPM,
     });
   }
 
-  SI.registerCallbacks(PIC, &FAM);
+  SI.registerCallbacks(PIC, &MAM);
 
   PipelineTuningOptions PTO;
   std::optional<PGOOptions> PGOOpt;
